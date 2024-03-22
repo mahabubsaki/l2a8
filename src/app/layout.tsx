6d10308bf0d7c4680095dabafa9c879e8df6d0de
mahabubsaki <md929820@gmail.com>
2024-03-22 23:54:08 +0600

    navbar created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/custom/ui/Navbar/Navbar";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
 
       <body className={inter.className}>
-
+        <Navbar />
         <main>
           {children}
         </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <section>
-
+      <p>test</p>
     </section>
   );
 }
